a9fb95dac8e27fda859f5d8bfbaa13a475715cd3
Rosmary Fermin <Rosmaryfc@hotmail.com>
2021-04-12 14:58:25 -0400

    verified password in login, verified email is unique in register

--- controllers/User.js
+++ controllers/User.js
@@ -24,7 +24,13 @@ class User {
     async register(req, res) {
         const user = req.body;
 
-        //check user has password
+        //check if email already exists
+        const getUser = await knex('users').where({email: user.email})
+        if(getUser.length !== 0) {
+            return res.status(500).json("user already exists");
+        };
+
+        //check if user has password
         if(!req.body.password) {
             return res.status(500).json ("no password created")
         }
@@ -36,13 +42,13 @@ class User {
 
         try{
             //hash password
-            const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-            user['password'] = hashedPassword;
+            await bcrypt.hash(req.body.password, 10).then(function(hash) {
+                user['password'] = hash;
+            });
 
             //create user in db
             //TODO: created edge cases for issue but why does this not allow data to be put in db if required field is not given? ex: no zipcode passed
             const createdUser = await knex('users').insert(user).returning(['id', 'email', 'username', 'zipcode']);
-
 
             //create a token
             const token = jwt.sign({
@@ -68,24 +74,40 @@ class User {
         try {
             //find user with email in db
             //TODO: why does this not throw an error when it returns nothing???? instead it returns []
-            const getUser = await knex('users').where({email: user.email}).select(['id', 'email', 'username', 'zipcode'])
+            let getUser = await knex('users').where({email: user.email})
 
-            if(getUser.id === null) {
-                return res.status(404).json("user not found")
+            console.log('getuser', getUser)
+
+            if(getUser.length === 0) {
+                return res.status(404).json("user not found");
+            };
+
+            console.log("hashed pw", getUser[0].password);
+            console.log("request password", user.password);
+
+            //verify password
+            const passwordIsValid = await bcrypt.compare(user.password, getUser[0].password);
+            console.log("passwordIsValid", passwordIsValid);
+            
+            if(!passwordIsValid) {
+                return res.status(500).json("incorrect password");
             }
 
-            //create a token
+            // create a token
             const token = jwt.sign({
-                id: user.id
+                id: getUser.id
             }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
             });
-            
-            res.status(200).json({user: getUser, auth: true, token:token});
 
+            // //remove password from obj
+            delete getUser[0].password;
+            console.log('getuser2', getUser)
+
+            res.status(200).json({user: getUser, auth: true, token:token});
         }catch (err) {
-            return res.status(500).json("error", {error: err} )
-        }
+            return res.status(500).json("error", {error: err} );
+        };
     };
 
     async logout(req, res) {
