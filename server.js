2fdf50aabc5daed6e556fc66154eb1b3039a0982
Rosmary Fermin <Rosmaryfc@hotmail.com>
2021-04-15 15:03:11 -0400

    added CORS for development and production

--- server.js
+++ server.js
@@ -2,13 +2,31 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const routes = require('./routes/index.js');
 
 //GLOBAL VARIABLES
 const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+
+//CORS SECURITY CONFIGURATIONS
+// CORS SECURITY CONFIGURATIONS
+const whitelist = ["http://localhost:3000/"]; // will add deployed links to array
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error("Not allowed by CORS, domain needs to be added to whitelist")
+      );
+    }
+  },
+};
+
 
 //MIDDLEWARE
-
+NODE_ENV === "development" ? app.use(cors()) : app.use(cors(corsOptions));
 //parses incoming requests with JSON
 app.use(express.json());
 //process form data
