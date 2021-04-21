1c6d4898f2bd37713602c37b993f07b016359537
phoenixmcknight <phoenixmcknight@gmail.com>
2021-04-21 13:28:58 -0400

    adds route for borough and position

--- migrations/20210409182639_create_locations_table.js
+++ migrations/20210409182639_create_locations_table.js
@@ -2,14 +2,13 @@
 exports.up = function(knex) {
     return knex.schema.createTable('locations', function (table) {
         table.increments('id')
-        table.integer('location_id').notNullable()
         table.string('api').notNullable()
         table.string('name').notNullable()
         table.string('city').notNullable()
         table.string('siteAddress').notNullable()
         table.string('zip').notNullable()
-        table.string('longitude')
-        table.string('latitude')
+        table.integer('longitude')
+        table.integer('latitude')
         table.string('accessibility')
         table.string('kosher')
         table.timestamps(true, true)
